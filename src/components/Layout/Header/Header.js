0dd95051c0bf0afd3c7ea620adebe7f03e219b66
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2023-01-12 20:46:02 +0300

    Adds Header component and its stylesheet

---
+++ src/components/Layout/Header/Header.js
@@ -0,0 +1,15 @@
+import classes from './Header.module.scss';
+
+import Logo from './Logo';
+import ProfileOptions from './ProfileOptions';
+
+const Header = props => {
+  return (
+    <header className={classes.header}>
+      <Logo />
+      <ProfileOptions />
+    </header>
+  );
+};
+
+export default Header;
