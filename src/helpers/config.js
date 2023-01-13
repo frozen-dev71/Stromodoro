d2a4f18f0cae19a9080cb69bf10d2b1f0bb6f7b4
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2023-01-13 20:57:58 +0300

    Creates helper config file to make easy for icons

---
+++ src/helpers/config.js
@@ -0,0 +1,14 @@
+export const taskCategories = [
+	{ name: 'Work', icon: '#icon-briefcase' },
+	{ name: 'Study', icon: '#icon-book' },
+	{ name: 'Exercise', icon: '#icon-dumbell' },
+	{ name: 'Health', icon: '#icon-aid-kit' },
+	{ name: 'Wellness', icon: '#icon-lotus' },
+	{ name: 'Chores', icon: '#icon-broom' },
+	{ name: 'Completed', icon: '#icon-checkmark' },
+	{ name: 'Ongoing', icon: '#icon-hour-glass' },
+	{ name: 'Show All', icon: '#icon-list' },
+  ];
+  
+  export const numDaysInMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  
