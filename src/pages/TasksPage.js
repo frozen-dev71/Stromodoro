3b4091d8010eac5fb9757f256d89679f138ccfbb
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2023-01-13 21:12:01 +0300

    Implements Tasks and Categories components into TasksPage

--- src/pages/TasksPage.js
+++ src/pages/TasksPage.js
@@ -1,9 +1,11 @@
-
+import Tasks from "../components/Tasks/Tasks";
+import Categories from "../components/Tasks/Categories";
 
 const TasksPage = () => {
   return (
     <div className="main-grid">
-
+      <Tasks />
+      <Categories />
     </div>
   );
 };
