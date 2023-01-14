fdad745c7dcd1ce885676fddbb18f95b8b74cf6a
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2023-01-14 21:29:06 +0300

    Imported all of the required pages from the main App.js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Layout from './components/Layout/Layout';
+import {
+  Routes,
+  Route,
+  Navigate,
+  matchPath,
+  useLocation,
+} from 'react-router-dom';
+import PomodoroPage from './pages/PomodoroPage';
+import DashboardPage from './pages/DashboardPage';
+import TasksPage from './pages/TasksPage';
+import { timerActions } from './store/timer';
+import { tasksActions } from './store/tasks';
+import { activityActions } from './store/activity';
+import { useDispatch, useSelector } from 'react-redux/es/exports';
+import { useEffect, useState } from 'react';
+import { dateIsYesterday, dateIsToday, getData } from './helpers/helpers';
+import { calendarActions } from './store/calendar';
+
 
 function App() {
   return (
