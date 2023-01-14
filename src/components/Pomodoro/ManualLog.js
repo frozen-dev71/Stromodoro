e3acd8faa2e9deb81116d84df5c274dfd376673d
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2023-01-14 21:03:16 +0300

    Adds Card Component to ManualLog component

--- src/components/Pomodoro/ManualLog.js
+++ src/components/Pomodoro/ManualLog.js
@@ -1,6 +1,6 @@
 import Card from '../UI/Card';
 import ConfirmAction from '../UserFeedback/ConfirmAction';
-
+import classes from './ManualLog.module.scss';
 import { useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { activityActions } from '../../store/activity';
@@ -87,7 +87,46 @@ const ManualLog = () => {
   };
 
   return (
-
+    <Card className={classes.manual}>
+      {confirmationData ? (
+        <ConfirmAction onClose={abortConfirmHandler} onConfirm={confirmHandler}>
+          {confirmationData.warning}
+        </ConfirmAction>
+      ) : (
+        ''
+      )}
+      <h3>Log productive time manually</h3>
+      <form className={classes.form} onSubmit={submitTimeHandler}>
+        <div className={classes.form__group}>
+          <label htmlFor="starting-hour">Starting Hour</label>
+          <input
+            required
+            type="number"
+            ref={startingHourRef}
+            min={5}
+            max={23}
+          />
+        </div>
+        <div className={classes.form__group}>
+          <label htmlFor="minutes-of-activity">Minutes of Activity</label>
+          <input
+            required
+            type="number"
+            ref={activeMinutesRef}
+            min={0}
+            id="minutes-of-activity"
+          />
+        </div>
+        <button type="submit" className="btn">
+          Submit
+        </button>
+        {error && (
+          <p className="error-message" style={{ gridColumn: '1 / -1' }}>
+            {error}
+          </p>
+        )}
+      </form>
+    </Card>
   );
 };
 
