036f2b0273a173ab206ce2f22f4016e027b76c18
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2023-01-16 00:33:41 +0300

    Update README.md

--- README.md
+++ README.md
@@ -1,70 +1,25 @@
-# Getting Started with Create React App
+# Stromodoro
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+<img width="1440" alt="Screenshot 2023-01-16 at 12 26 20 AM" src="https://user-images.githubusercontent.com/44535117/212568200-61be3f93-f633-4c94-b420-1ac2a6f6e1c0.png?raw=true">
 
-## Available Scripts
 
-In the project directory, you can run:
+## Features
++ [X] Pomodoro Timer.
++ [X] Adjust Pomodoro timing.
++ [X] Add task with category.
++ [X] Filter by task.
++ [X] See your statistics weekly, monthly.
++ [X] Show your Streaks.
++ [X] Local Storage.
++ [X] Chart View.
++ [X] Responsive.
 
-### `npm start`
+### Built With
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+This app is built with Reactjs and . It uses webpack as module bundler and NPM as package manager.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+- [Reactjs](https://reactjs.org/)
+- [Redux](https://redux.js.org/)
+- [SASS](https://sass-lang.com/)
+- [Webpack](https://webpack.js.org/)
+- [NPM](https://www.npmjs.com/)
--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import Layout from './components/Layout/Layout';
+import Layout from "./components/Layout/Layout";
 import {
   Routes,
   Route,
   Navigate,
   matchPath,
   useLocation,
-} from 'react-router-dom';
-import PomodoroPage from './pages/PomodoroPage';
-import DashboardPage from './pages/DashboardPage';
-import StatisticsPage from './pages/StatisticsPage';
-import TasksPage from './pages/TasksPage';
-import { timerActions } from './store/timer';
-import { tasksActions } from './store/tasks';
-import { activityActions } from './store/activity';
-import { useDispatch, useSelector } from 'react-redux/es/exports';
-import { useEffect, useState } from 'react';
-import useUnload from './hooks/useUnload';
-import usePageVisibilty from './hooks/usePageVisibility';
-import { dateIsYesterday, dateIsToday, getData } from './helpers/helpers';
-import { calendarActions } from './store/calendar';
-import audioFile from './assets/completed.mp3';
-
+} from "react-router-dom";
+import PomodoroPage from "./pages/PomodoroPage";
+import DashboardPage from "./pages/DashboardPage";
+import StatisticsPage from "./pages/StatisticsPage";
+import TasksPage from "./pages/TasksPage";
+import { timerActions } from "./store/timer";
+import { tasksActions } from "./store/tasks";
+import { activityActions } from "./store/activity";
+import { useDispatch, useSelector } from "react-redux/es/exports";
+import { useEffect, useState } from "react";
+import useUnload from "./hooks/useUnload";
+import usePageVisibilty from "./hooks/usePageVisibility";
+import { dateIsYesterday, dateIsToday, getData } from "./helpers/helpers";
+import { calendarActions } from "./store/calendar";
+import audioFile from "./assets/completed.mp3";
+import { Analytics } from "@vercel/analytics/react";
 
 let secondsOutsidePomodoro = 0;
 let audioPlayedOutside = false;
@@ -28,18 +28,18 @@ const audio = new Audio(audioFile);
 
 function App() {
   const dispatch = useDispatch();
-  const timerIsActive = useSelector(state => state.timer.isActive);
+  const timerIsActive = useSelector((state) => state.timer.isActive);
   const location = useLocation();
-  const pomodoroWasCompleted = useSelector(state => state.timer.wasCompleted);
-  const pomodoroMinutes = useSelector(state => state.timer.config.pomodoro);
-  const countdown = useSelector(state => state.timer.countdown);
+  const pomodoroWasCompleted = useSelector((state) => state.timer.wasCompleted);
+  const pomodoroMinutes = useSelector((state) => state.timer.config.pomodoro);
+  const countdown = useSelector((state) => state.timer.countdown);
   const [pageVisibility, setPageVisibility] = useState(
     document.visibilityState
   );
 
   const match = matchPath(
     {
-      path: '/pomodoro',
+      path: "/pomodoro",
       exact: true,
       strict: false,
     },
@@ -47,22 +47,22 @@ function App() {
   );
 
   useEffect(() => {
-    if (localStorage.getItem('timer')) {
+    if (localStorage.getItem("timer")) {
       dispatch(timerActions.getTimerData());
     }
-    if (localStorage.getItem('tasks')) {
+    if (localStorage.getItem("tasks")) {
       dispatch(tasksActions.getTasksData());
     }
-    if (localStorage.getItem('calendar')) {
+    if (localStorage.getItem("calendar")) {
       dispatch(calendarActions.getCalendarData());
     }
-    if (localStorage.getItem('activity')) {
-      const storedActivity = getData('activity');
+    if (localStorage.getItem("activity")) {
+      const storedActivity = getData("activity");
       if (dateIsToday(storedActivity.date)) {
         dispatch(activityActions.getActivityData());
       } else {
         dispatch(calendarActions.insertActivityData(storedActivity));
-        dispatch(timerActions.changeTimer('pomodoro'));
+        dispatch(timerActions.changeTimer("pomodoro"));
         if (!dateIsYesterday(storedActivity.date))
           // insert null data for yesterday so the statistics will update with the correct data
           dispatch(calendarActions.insertYesterdayData());
@@ -82,7 +82,6 @@ function App() {
       );
     }
   }, [pomodoroWasCompleted, dispatch, pomodoroMinutes]);
-
 
   useEffect(() => {
     if (!timerIsActive) return;
@@ -113,19 +112,17 @@ function App() {
     if (remainingSeconds === 0 && !audioPlayedOutside) audio.play();
   }, [countdown]);
 
-
-
   useEffect(() => {
-    if (document.visibilityState === 'hidden' && timerIsActive) {
+    if (document.visibilityState === "hidden" && timerIsActive) {
       dispatch(timerActions.toggleIsActive());
-      localStorage.setItem('startOfInactive', JSON.stringify(Date.now()));
+      localStorage.setItem("startOfInactive", JSON.stringify(Date.now()));
     }
     if (
-      document.visibilityState === 'visible' &&
-      localStorage.getItem('startOfInactive')
+      document.visibilityState === "visible" &&
+      localStorage.getItem("startOfInactive")
     ) {
       const storedTimestamp = JSON.parse(
-        localStorage.getItem('startOfInactive')
+        localStorage.getItem("startOfInactive")
       );
       const secondsPassed = Math.trunc((Date.now() - storedTimestamp) / 1000);
 
@@ -138,7 +135,7 @@ function App() {
         );
         secondsOutsidePomodoro = 0;
       }
-      localStorage.removeItem('startOfInactive');
+      localStorage.removeItem("startOfInactive");
     }
   }, [pageVisibility, dispatch, timerIsActive]);
 
@@ -146,23 +143,23 @@ function App() {
     setPageVisibility(document.visibilityState);
   });
 
-  useUnload(event => {
+  useUnload((event) => {
     event.preventDefault();
     dispatch(timerActions.subtractOutsideSeconds(secondsOutsidePomodoro));
     secondsOutsidePomodoro = 0;
   });
 
-
   return (
     <Layout>
-    <Routes>
-      <Route path="/" element={<Navigate to="/dashboard" replace />} />
-      <Route path="/pomodoro" element={<PomodoroPage />} />
-      <Route path="/tasks" element={<TasksPage />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
-      <Route path="/statistics" element={<StatisticsPage />} />
-    </Routes>
-  </Layout>
+      <Analytics />
+      <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+        <Route path="/pomodoro" element={<PomodoroPage />} />
+        <Route path="/tasks" element={<TasksPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/statistics" element={<StatisticsPage />} />
+      </Routes>
+    </Layout>
   );
 }
 
